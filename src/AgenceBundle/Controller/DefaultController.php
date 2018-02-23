2618bf4c1980029aa4998981b8805197e448147a
Yassin06 <34058877+Yassin06@users.noreply.github.com>
2018-02-24 00:53:44 +0100

    Created project

---
+++ src/AgenceBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AgenceBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('AgenceBundle:Default:index.html.twig');
+    }
+}
